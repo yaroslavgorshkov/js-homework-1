ee24aa3192b762f7119850e87f70c936146dd2b3
yaroslavgorshkov <neznajkinarik@gmail.com>
2025-03-25 20:26:03 +0200

    feat: smth

--- hw/script.js
+++ hw/script.js
@@ -1,9 +1,11 @@
-const paragraphs = document.getElementsByTagName("p");
-const myButton = document.querySelector("button");
-const paragraphsArray = Array.from(paragraphs);
-myButton.addEventListener("click", () => {
-    for(let i = 0; i < paragraphsArray.length; i++) {
-        const bufData = paragraphsArray[i].innerText;
-        paragraphsArray[i].innerText = bufData + " " + (i+1);
-    }
+document.addEventListener("DOMContentLoaded", () => {
+    const paragraphs = document.getElementsByTagName("p");
+    const myButton = document.querySelector("button");
+    const paragraphsArray = Array.from(paragraphs);
+    myButton.addEventListener("click", () => {
+        for(let i = 0; i < paragraphsArray.length; i++) {
+            const bufData = paragraphsArray[i].innerText;
+            paragraphsArray[i].innerText = `${bufData} ${i+1}`;
+        }
+    })
 })
